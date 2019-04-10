53104731e3d5934b58108e2fef07cbb79b5367c2
Mark <meichangliang@outlook.com>
2019-04-10 15:01:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,36 +1,42 @@
 const config = {
-  projectName: 'myapp',
+  projectName: 'one-taro-app',
   date: '2019-4-5',
   designWidth: 750,
   deviceRatio: {
     '640': 2.34 / 2,
     '750': 1,
-    '828': 1.81 / 2
+    '828': 1.81 / 2,
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
   plugins: {
+    csso: {
+      enable: true,
+      config: {
+        // 配置项同 https://github.com/css/csso#minifysource-options
+      },
+    },
     babel: {
       sourceMap: true,
       presets: [
         [
           'env',
           {
-            modules: false
-          }
-        ]
+            modules: false,
+          },
+        ],
       ],
       plugins: [
         'transform-decorators-legacy',
         'transform-class-properties',
-        'transform-object-rest-spread'
-      ]
-    }
+        'transform-object-rest-spread',
+      ],
+    },
   },
   defineConstants: {},
   copy: {
     patterns: [],
-    options: {}
+    options: {},
   },
   weapp: {
     module: {
@@ -38,28 +44,28 @@ const config = {
         autoprefixer: {
           enable: true,
           config: {
-            browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8']
-          }
+            browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8'],
+          },
         },
         pxtransform: {
           enable: true,
-          config: {}
+          config: {},
         },
         url: {
           enable: true,
           config: {
-            limit: 10240 // 设定转换尺寸上限
-          }
+            limit: 10240, // 设定转换尺寸上限
+          },
         },
         cssModules: {
           enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
           config: {
             namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
-        }
-      }
-    }
+            generateScopedName: '[name]__[local]___[hash:base64:5]',
+          },
+        },
+      },
+    },
   },
   h5: {
     publicPath: '/',
@@ -69,19 +75,19 @@ const config = {
         autoprefixer: {
           enable: true,
           config: {
-            browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8']
-          }
+            browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8'],
+          },
         },
         cssModules: {
           enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
           config: {
             namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
-        }
-      }
-    }
-  }
+            generateScopedName: '[name]__[local]___[hash:base64:5]',
+          },
+        },
+      },
+    },
+  },
 };
 
 module.exports = function(merge) {
