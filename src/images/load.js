0987be9f566ca13768d3746cf89e59bdd8a2e88d
Mark <meichangliang@outlook.com>
2019-04-26 17:54:20 +0800

    ~

--- src/images/load.js
+++ src/images/load.js
@@ -3,7 +3,7 @@
  * @Description: In User Settings Edit
  * @Author: Mark
  * @Date: 2019-04-26 16:59:51
- * @LastEditTime: 2019-04-26 17:41:46
+ * @LastEditTime: 2019-04-26 17:54:14
  */
 
 import { image_url } from '../../config/baseUrl';
